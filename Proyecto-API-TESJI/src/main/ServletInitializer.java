77701b0fa856ff09f1d02149bd6af94ebf66ef1d
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-06-11 21:17:22 -0600

    Add files via upload

---
+++ Proyecto-API-TESJI/src/main/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tesji.proyectoapi.Proyecto.API.TESJI;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProyectoApiTesjiApplication.class);
+	}
+
+}
